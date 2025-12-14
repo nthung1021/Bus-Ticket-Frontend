e5c8dcc6282f716201e5c2263fae7f2bc98ac5ad
codefulTM <147736560+codefulTM@users.noreply.github.com>
2025-12-15 04:04:29 +0700

    made the booking real time

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode, useState } from "react";
 import { ThemeProvider } from "src/components/ThemeProvider";
 import { SeatProvider } from "@/contexts/seat-context";
+import { BookingProvider } from "@/contexts/booking-context";
 import { Toaster } from "react-hot-toast";
 
 export function Providers({ children }: { children: ReactNode }) {
@@ -16,15 +17,17 @@ export function Providers({ children }: { children: ReactNode }) {
             refetchOnWindowFocus: false,
           },
         },
-      }),
+      })
   );
 
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
         <SeatProvider>
-          <Toaster />
-          {children}
+          <BookingProvider>
+            <Toaster />
+            {children}
+          </BookingProvider>
         </SeatProvider>
       </ThemeProvider>
     </QueryClientProvider>
